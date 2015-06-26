bb7d4405a8df56260c19e83d012635a5e5e55f88
root <root@localhost>
2015-06-26 10:08:14 +0200

    Add benchmark for Heap iterator with an array.

--- benches/bench.rs
+++ benches/bench.rs
@@ -21,6 +21,17 @@ fn heap_iterative_7(b: &mut Bencher) {
 }
 
 #[bench]
+fn heap_iterative_7_iter(b: &mut Bencher) {
+    let mut data = [0; 7];
+    b.iter(|| {
+        let heap = Heap::new(&mut data);
+        for elt in heap {
+            black_box(elt[0]);
+        }
+    });
+}
+
+#[bench]
 fn heap_recursive_7(b: &mut Bencher) {
     let mut data = [0; 7];
     b.iter(|| {
